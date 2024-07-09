f5ae0cb47dbe5b73a8b849764ed8bb5b16600480
houssamedine <houssamedineelazhari@gmail.com>
2024-07-09 11:25:59 +0100

    change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default  function  App() {
           <h2>Hi from some where, we well add navbar</h2>
           <h2>Different Section</h2>
           <h2>Mobile View</h2>
+          <h2>Other Things</h2>
       </>
   );
 }
