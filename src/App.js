3ce13dbe6168e93452bab8d9ac2cbb0409238bc7
houssamedine <houssamedineelazhari@gmail.com>
2024-07-09 12:05:19 +0100

    finaliser navbar branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Navbar from "./components/navbar/Navbar";
 
 export default  function  App() {
   return (
       <>
-          <h2>Hi from some where, we well add navbar</h2>
-          <h2>Different Section</h2>
-          <h2>Mobile View</h2>
-          <h2>Other Things</h2>
+          <Navbar/>
       </>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import  "./Navbar.css"
+export default function  Navbar () {
+    return  <>
+        <nav className="navbar-main">
+            <ul>
+                <li className="nav-item">Home</li>
+                <li className="nav-item">About</li>
+                <li className="nav-item">Service</li>
+                <li className="nav-item">Carer</li>
+                <li className="nav-item">Video</li>
+                <li className="nav-item">Team</li>
+                <li className="nav-item">Testimonial</li>
+                <li className="nav-item">Feedback</li>
+                <li className="nav-item">Contact</li>
+                <li className="nav-item">Subscription</li>
+            </ul>
+        </nav>
+    </>
+
+}
