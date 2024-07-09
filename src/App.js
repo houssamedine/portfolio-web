d19a573cc3babb01271db0818d2a123c80fdb793
houssamedine <houssamedineelazhari@gmail.com>
2024-07-09 11:13:35 +0100

    change text

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 export default  function  App() {
   return (
-    <div className="App">
-          <h1>Hi from some where, we well add navbar</h1>
-    </div>
+      <>
+          <h2>Hi from some where, we well add navbar</h2>
+          <h2>Different Section</h2>
+          <h2>Mobile View</h2>
+      </>
   );
 }
 
